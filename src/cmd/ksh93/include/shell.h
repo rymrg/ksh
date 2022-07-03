rymrg/ksh

commit b3050769ea281f5ff49feed6d4b1354e8db5d68a

Fix 'return' emitting signals; allow arbitrary return values
When a global EXIT trap is set, and a ksh-style function exits with
a status > 256 that could have been the result of a signal, then
the shell incorrectly issues that signal to itself. Depending on
the signal, this causes ksh to terminate itself ungracefully:

    $ cat /tmp/exit267
    trap 'echo OK' EXIT  # This trap triggers the crash
    function foo { return 267; }
    foo
    $ bash /tmp/exit267
    OK
    $ ksh-3aee10d7 /tmp/exit267
    OK
    $ ksh /tmp/exit267
    Memory fault(coredump)

On most systems, status 267 corresponds to SIGSEGV. The reported
memory fault is not real; it results from ksh incorrectly killing
itself with that signal.

The problem is caused by two factors:

1. As of 93u+ 2012-08-01, ksh explicitly allows 'return' to use an
   exit status corresponding to a signal (from 257 to end of signal
   range). The rest of the integer range is trunctated to 8 bits.
   This is contrary to both 'man ksh' and 'return --man' which both
   say it's always truncated to 8 bits. Plus, combined with point 2
   below, this new behaviour is nonsensical, as 'return' has no
   business actually generating signals. However, a couple of
   regression tests now depend on this, as may some scripts.

2. When a ksh-style function does not handle a signal, the signal
   is passed down to the parent environment and ksh does this by
   reissuing the signal to its own process after leaving the
   function scope. However, it does this by checking the exit
   status, which is very bad practice as there is no guarantee
   that an exit status corresponding to a signal was in fact
   produced by a signal, particularly after they changed the
   behaviour of 'return' per 1 above.

This commit fixes both issues. It also takes a proper decision on
allowable 'return' exit status arguments. Since 93u+ was released
nearly a decade ago and some scripts may now rely on being able to

src/cmd/ksh93/include/version.h
@@ -21,7 +21,7 @@
 
 #define SH_RELEASE_FORK	"93u+m"		/* only change if you develop a new ksh93 fork */
 #define SH_RELEASE_SVER	"1.0.0-beta.2"	/* semantic version number: https://semver.org */
-#define SH_RELEASE_DATE	"2021-12-05"	/* must be in this format for $((.sh.version)) */
+#define SH_RELEASE_DATE	"2021-12-08"	/* must be in this format for $((.sh.version)) */
 #define SH_RELEASE_CPYR	"(c) 2020-2021 Contributors to ksh " SH_RELEASE_FORK
 
 /* Scripts sometimes field-split ${.sh.version}, so don't change amount of whitespace. */

src/cmd/ksh93/sh/subshell.c
@@ -871,6 +871,9 @@ Sfio_t *sh_subshell(Shell_t *shp,Shnode_t *t, volatile int flags, int comsub)
 			srand(rp->rand_seed = sp->rand_seed);
 			rp->rand_last = sp->rand_last;
 		}
+		/* Real subshells have their exit status truncated to 8 bits by the kernel.
+		 * Since virtual subshells should be indistinguishable, do the same here. */
+		sh.exitval &= SH_EXITMASK;
 	}
 	shp->subshare = sp->subshare;
 	shp->subdup = sp->subdup;

src/cmd/ksh93/sh/xec.c
@@ -3259,8 +3259,8 @@ int sh_funscope(int argn, char *argv[],int(*fun)(void*),void *arg,int execflg)
 	}
 	if(jmpval)
 		r=shp->exitval;
-	if(nsig && r>SH_EXITSIG)
-		kill(shgd->current_pid,r&SH_EXITMASK);
+	if(jmpval==SH_JMPFUN && sh.lastsig)
+		kill(shgd->current_pid, sh.lastsig);  /* pass down unhandled signal that interrupted ksh function */
 	if(jmpval > SH_JMPFUN)
 	{
 		sh_chktrap(shp);

src/cmd/ksh93/tests/subshell.sh
@@ -1084,4 +1084,11 @@ then	kill -9 $tpid
 fi
 
 # ======
+# Virtual subshells should clip $? to 8 bits, as real subshells get that enforced by the kernel.
+# (Note: 'ulimit' will reliably fork a virtual subshell into a real one.)
+e1=$( (f() { return 267; }; f); echo $? )
+e2=$( (ulimit -t unlimited 2>/dev/null; f() { return 267; }; f); echo $? )
+((e1==11 && e2==11)) || err_exit "exit status of virtual ($e1) and real ($e2) subshell should both be clipped to 8 bits (11)"
+
+# ======
 exit $((Errors<125?Errors:125))

src/cmd/ksh93/COMPATIBILITY
@@ -164,6 +164,10 @@ For more details, see the NEWS file and for complete details, see the git log.
 29.	It is now an error for arithmetic expressions to assign an out-of-range
 	index value to a variable of an enumeration type created with 'enum'.
 
+30.	For the 'return' built-in command, you can now freely specify any
+	return value that fits in a signed integer, typically a 32-bit value.
+	Note that $? is truncated to 8 bits when the current (sub)shell exits.
+
 ____________________________________________________________________________
 
 		KSH-93 VS. KSH-88

src/cmd/ksh93/bltins/cflow.c
@@ -46,11 +46,10 @@
 #endif
 int	b_return(register int n, register char *argv[],Shbltin_t *context)
 {
-	register char *arg;
-	register Shell_t *shp = context->shp;
-	struct checkpt *pp = (struct checkpt*)shp->jmplist;
-	const char *options = (**argv=='r'?sh_optreturn:sh_optexit);
-	while((n = optget(argv,options))) switch(n)
+	/* 'return' outside of function, dotscript and profile behaves like 'exit' */
+	char do_exit = **argv=='e' || sh.fn_depth==0 && sh.dot_depth==0 && !sh_isstate(SH_PROFILE);
+	NOT_USED(context);
+	while((n = optget(argv, **argv=='e' ? sh_optexit : sh_optreturn))) switch(n)
 	{
 	    case ':':
 		if(!strmatch(argv[opt_info.index],"[+-]+([0-9])"))
@@ -66,17 +65,27 @@ done:
 		errormsg(SH_DICT,ERROR_usage(2),"%s",optusage((char*)0));
 		UNREACHABLE();
 	}
-	pp->mode = (**argv=='e'?SH_JMPEXIT:SH_JMPFUN);
 	argv += opt_info.index;
-	n = (arg = *argv) ? (int)strtol(arg, (char**)0, 10) : shp->savexit;
-	if(n<0 || n==256 || n > SH_EXITMASK+shp->gd->sigmax+1)
-			n &= ((unsigned int)n)&SH_EXITMASK;
-	/* return outside of function, dotscript and profile is exit */
-	if(shp->fn_depth==0 && shp->dot_depth==0 && !sh_isstate(SH_PROFILE))
-		pp->mode = SH_JMPEXIT;
-	shp->savexit = n;
-	sh_exit((pp->mode == SH_JMPEXIT) ? (n & SH_EXITMASK) : n);
-	return(1);
+	if(*argv)
+	{
+		long l = strtol(*argv, NIL(char**), 10);
+		if(do_exit)
+			n = (int)(l & SH_EXITMASK);	/* exit: apply bitmask before conversion to avoid undefined int overflow */
+		else if((long)(n = (int)l) != l)	/* return: convert to int and check for overflow (should be safe enough) */
+		{
+			errormsg(SH_DICT,ERROR_warn(0),"%s: out of range",*argv);
+			n = 128;			/* overflow is undefined, so use a consistent status for this */
+		}
+	}
+	else
+	{
+		n = sh.savexit;				/* no argument: pass down $? */
+		if(do_exit)
+			n &= SH_EXITMASK;
+	}
+	((struct checkpt*)sh.jmplist)->mode = do_exit ? SH_JMPEXIT : SH_JMPFUN;
+	sh_exit(sh.savexit = n);
+	UNREACHABLE();
 }
 
 

src/cmd/ksh93/data/builtins.c
@@ -637,7 +637,7 @@ const char sh_optexec[] =
 ;
 
 const char sh_optexit[] =
-"[-1c?\n@(#)$Id: exit (AT&T Research) 1999-07-07 $\n]"
+"[-1c?\n@(#)$Id: exit (ksh 93u+m) 2021-12-08 $\n]"
 "[--catalog?" SH_DICT "]"
 "[+NAME?exit - exit the current shell]"
 "[+DESCRIPTION?\bexit\b is shell special built-in that causes the "
@@ -647,10 +647,10 @@ const char sh_optexit[] =
 "\n"
 "\n[n]\n"
 "\n"
-"[+EXIT STATUS?If \an\a is specified, the exit status is the least significant "
-	"eight bits of the value of \an\a.  Otherwise, the exit status is the "
-	"exit status of preceding command.  When invoked inside a trap, the "
-	"preceding command means the command that invoked the trap.]"
+"[+EXIT STATUS?The exit status is the least significant eight bits of the "
+	"value of \an\a (if specified) or of the exit status of the preceding "
+	"command. If \bexit\b is invoked inside a trap, the preceding command "
+	"means the command that invoked the trap.]"
 "[+SEE ALSO?\bbreak\b(1), \breturn\b(1)]"
 ;
 
@@ -1515,25 +1515,28 @@ const char sh_optredirect[] =
 ;
 
 const char sh_optreturn[] =
-"[-1c?\n@(#)$Id: return (AT&T Research) 1999-07-07 $\n]"
+"[-1c?\n@(#)$Id: return (ksh 93u+m) 2021-12-08 $\n]"
 "[--catalog?" SH_DICT "]"
 "[+NAME?return - return from a function or dot script ]"
 "[+DESCRIPTION?\breturn\b is a shell special built-in that causes the "
-	"function or dot script that invokes it to exit.  "
-	"If \breturn\b is invoked outside of a function or dot script "
-	"it is equivalent to \bexit\b.]"
+	"function, dot script or profile script that invokes it to exit. "
+	"If \breturn\b is invoked outside of one of these, it behaves "
+	"exactly like \bexit\b(1); see its manual page.]"
 "[+?If \breturn\b is invoked inside a function defined with the \bfunction\b "
 	"reserved word syntax, then any \bEXIT\b trap set within the "
-	"then function will be invoked in the context of the caller "
+	"function will be invoked in the context of the caller "
 	"before the function returns.]"
 "[+?If \an\a is given, it will be used to set the exit status.]"
 "\n"
 "\n[n]\n"
 "\n"
-"[+EXIT STATUS?If \an\a is specified, the exit status is the least significant "
-	"eight bits of the value of \an\a.  Otherwise, the exit status is the "
-	"exit status of preceding command.]"
-"[+SEE ALSO?\bbreak\b(1), \bexit\b(1)]"
+"[+EXIT STATUS?If \an\a is not specified, the exit status is that of the "
+	"preceding command. Otherwise, it is the value \an\a as a signed "
+	"integer. An out-of-range value produces a warning and an exit "
+	"status of 128. The range can be shown using \bgetconf INT_MIN\b "
+	"and \bgetconf INT_MAX\b. When the current (sub)shell exits, "
+	"the exit status is truncated to 8 bits as in \bexit\b.]"
+"[+SEE ALSO?\bbreak\b(1), \bexit\b(1), \bgetconf\b(1)]"
 ;
 
 

src/cmd/ksh93/sh.1
@@ -1523,10 +1523,21 @@ the
 command.
 .TP
 .B ?
-The decimal value returned by the last executed command.
+The exit status returned by the last executed command. Its meaning depends
+on the command or function that defines it, but there are conventions that
+other commands often depend on: zero typically means 'success' or 'true',
+one typically means 'non-success' or 'false', and a value greater than one
+typically indicates some kind of error. Only the 8 least significant bits of
+\f3$?\fP (values 0 to 255) are preserved when the exit status is passed on
+to a parent process, but within the same (sub)shell environment, it is a
+signed integer value with a range of possible values as shown by the
+commands \f3getconf INT_MIN\fP and \f3getconf INT_MAX\fP. Shell functions
+that run in the current environment may return status values in this range.
 .TP
 .B $
-The process id of this shell.
+The process ID of the main shell process. Note that this value will not
+change in a subshell, even if the subshell runs in a different process.
+See also \f3.sh.pid\fP.
 .TP
 .B _
 Initially, the value of
@@ -6060,17 +6071,15 @@ then this command persistently modifies file descriptors as in
 Causes the shell to exit
 with the exit status specified by
 .IR n .
-The value will be the least significant 8 bits of the specified status.
-If
+The value will be the least significant 8 bits of
 .I n\^
-is omitted, then the exit status is that of the last command executed.
-An end-of-file will also cause the shell to exit
-except for a
-shell which has the
+(if specified) or of the exit status of the last command executed.
+An end-of-file will also cause the shell to exit,
+except for an interactive shell that has the
 .B ignoreeof
-option (see
+option turned on (see
 .B set
-below) turned on.
+below).
 .TP
 \(dg\(dd \f3export\fP \*(OK \f3\-p\fP \*(CK \*(OK \f2name\^\fP\*(OK\f3=\fP\f2value\^\fP\*(CK \*(CK .\|.\|.
 If
@@ -6959,25 +6968,20 @@ redirected to themselves as part of the invocation (e.g. \fB4>&4\fR)
 or if the \fBposix\fR option is set.
 .TP
 \(dg \f3return\fP \*(OK \f2n\^\fP \*(CK
-Causes a shell
-.I function
-or
-\f3\|.\fP
-script to return
-to the invoking script
-with the exit status specified by
+Causes a shell function, dot script (see \f3\|.\fP and \f3source\fP),
+or profile script to return to the invoking shell environment with the
+exit status specified by
 .IR n .
-The value will be the least significant 8 bits of the specified status.
+This status value can use the full signed integer range as shown by the
+commands \f3getconf INT_MIN\fP and \f3getconf INT_MAX\fP. A value
+outside that range will produce a warning and an exit status of 128.
 If
 .I n\^
-is omitted, then the return status is that of the last command executed.
+is omitted, then the value of \f3$?\fP is assumed, i.e., the exit
+status of the last command executed is passed on.
 If
 .B return
-is invoked while not in a
-.I function
-or a
-\f3\|.\fP
-script,
+is invoked while not in a function, dot script, or profile script,
 then it behaves the same as
 .BR exit .
 .TP
@@ -7169,7 +7173,7 @@ Same as
 .BR \-H .
 .TP 8
 .B ignoreeof
-The shell will not exit on end-of-file.
+An interactive shell will not exit on end-of-file.
 The command
 .B exit
 must be used.

commit 4df6d674a08c7ca92e70e2c44b9ebd3756071719

Fix signal exit status of last command in subshell (re: b3050769)
Reproducer (on macOS/*BSD where SIGUSR1 has signal number 30):

  $ ksh -c '(sh -c '\''kill -s USR1 $$'\''); echo $?'
  ksh: 54220: User signal 1
  30

Expected output for $?: 286, not 30. The signal is not reflected in
the 9th bit of the exit status.

This bug was introduced for virtual subshells in b3050769 but
exists in every ksh93 version for real (forked) subshells:

  $ ksh -c '(ulimit -t unlimited; trap : EXIT; \
	sh -c '\''kill -s USR1 $$'\''); echo $?'
  ksh: 54267: User signal 1
  30

(As of d6c9821c, a dummy trap is needed to trigger the bug, or it
will be masked by the exec optimization for the sh invocation.)

This is caused by the exit status being masked to 8 bits when a
subshell terminates. For a real subshell, this is inevitable as the
kernel does this. As of b3050769, virtual subshells behave in a
manner consistent with real subshells in this regard.

However, for both virtual and real subshells, if its last command
was terminated by a signal, then that should still be reflected in
the 9th bit of ksh's exit stauts.

The root of the problem is that ksh simply cannot rely internally
on the 9th bit of the exit status to determine if a command exited
due to a signal. The 9th bit may be trimmed by a subshell or may be
set by 'return' without a signal being involved. This commit fixes
it by introducing a separate flag which will be a reliable
indicator of this.

src/cmd/ksh93/include/shell.h:
- Add sh.chldexitsig flag (set if the last command was a child
  process that exited due to a signal).

src/cmd/ksh93/sh/jobs.c: job_wait():
- When the last child process exited due to a signal, not only set
  the 9th (SH_EXITSIG) bit of sh.exitval but also sh.chldexitsig.

src/cmd/ksh93/sh/subshell.c: sh_subshell():
- Fix the virtual subshell reproducer above. After trimming the
  exit status to 8 bit, set the 9th bit if sh.chldexitsig is set.

src/cmd/ksh93/include/shell.h
@@ -319,6 +319,7 @@ struct Shell_s
 	char		indebug; 	/* set when in debug trap */
 	unsigned char	ignsig;		/* ignored signal in subshell */
 	unsigned char	lastsig;	/* last signal received */
+	char		chldexitsig;	/* set if the last command was a child process that exited due to a signal */
 	char		pathinit;	/* pathinit called from subshell */
 	char		comsub;		/* set to 1 when in `...`, 2 when in ${ ...; }, 3 when in $(...) */
 	char		subshare;	/* set when comsub==2 (shared-state ${ ...; } command substitution) */

src/cmd/ksh93/include/version.h
@@ -23,7 +23,7 @@
 
 #define SH_RELEASE_FORK	"93u+m"		/* only change if you develop a new ksh93 fork */
 #define SH_RELEASE_SVER	"1.0.0-beta.2"	/* semantic version number: https://semver.org */
-#define SH_RELEASE_DATE	"2022-07-01"	/* must be in this format for $((.sh.version)) */
+#define SH_RELEASE_DATE	"2022-07-02"	/* must be in this format for $((.sh.version)) */
 #define SH_RELEASE_CPYR	"(c) 2020-2022 Contributors to ksh " SH_RELEASE_FORK
 
 /* Scripts sometimes field-split ${.sh.version}, so don't change amount of whitespace. */

src/cmd/ksh93/sh/fault.c
@@ -482,6 +482,7 @@ int sh_trap(const char *trap, int mode)
 	int	jmpval, savxit = sh.exitval, savxit_return;
 	int	was_history = sh_isstate(SH_HISTORY);
 	int	was_verbose = sh_isstate(SH_VERBOSE);
+	char	save_chldexitsig = sh.chldexitsig;
 	int	staktop = staktell();
 	char	*savptr = stakfreeze(0);
 	struct	checkpt buff;
@@ -529,6 +530,7 @@ int sh_trap(const char *trap, int mode)
 		sh_onstate(SH_HISTORY);
 	if(was_verbose)
 		sh_onstate(SH_VERBOSE);
+	sh.chldexitsig = save_chldexitsig;
 	exitset();
 	if(jmpval>SH_JMPTRAP && (((struct checkpt*)sh.jmpbuffer)->prev || ((struct checkpt*)sh.jmpbuffer)->mode==SH_JMPSCRIPT))
 		siglongjmp(*sh.jmplist,jmpval);
@@ -682,7 +684,7 @@ noreturn void sh_done(register int sig)
 	sfsync((Sfio_t*)sfstdin);
 	sfsync((Sfio_t*)sh.outpool);
 	sfsync((Sfio_t*)sfstdout);
-	if(savxit&SH_EXITSIG && (savxit&SH_EXITMASK) == sh.lastsig)
+	if((sh.chldexitsig && sh.realsubshell) || (savxit&SH_EXITSIG && (savxit&SH_EXITMASK) == sh.lastsig))
 		sig = savxit&SH_EXITMASK;
 	if(sig)
 	{
@@ -707,10 +709,8 @@ noreturn void sh_done(register int sig)
 	if(sh_isoption(SH_NOEXEC))
 		kiaclose((Lex_t*)sh.lex_context);
 #endif /* SHOPT_KIA */
-
 	/* Exit with portable 8-bit status (128 + signum) if last child process exits due to signal */
-	if (savxit & SH_EXITSIG)
+	if(sh.chldexitsig)
 		savxit = savxit & ~SH_EXITSIG | 0200;
-
 	exit(savxit&SH_EXITMASK);
 }

src/cmd/ksh93/sh/jobs.c
@@ -1508,7 +1508,10 @@ int	job_wait(register pid_t pid)
 					{
 						sh.exitval=px->p_exit;
 						if(px->p_flag&P_SIGNALLED)
+						{
 							sh.exitval |= SH_EXITSIG;
+							sh.chldexitsig = 1;
+						}
 						if(intr)
 							px->p_flag &= ~P_EXITSAVE;
 					}

src/cmd/ksh93/sh/subshell.c
@@ -680,6 +680,8 @@ Sfio_t *sh_subshell(Shnode_t *t, volatile int flags, int comsub)
 		if(jmpval==SH_JMPSCRIPT)
 			siglongjmp(*sh.jmplist,jmpval);
 		sh.exitval &= SH_EXITMASK;
+		if(sh.chldexitsig)
+			sh.exitval |= SH_EXITSIG;
 		sh_done(0);
 	}
 	if(!sh.savesig)
@@ -873,6 +875,8 @@ Sfio_t *sh_subshell(Shnode_t *t, volatile int flags, int comsub)
 		/* Real subshells have their exit status truncated to 8 bits by the kernel.
 		 * Since virtual subshells should be indistinguishable, do the same here. */
 		sh.exitval &= SH_EXITMASK;
+		if(sh.chldexitsig)
+			sh.exitval |= SH_EXITSIG;
 	}
 	sh.subshare = sp->subshare;
 	sh.subdup = sp->subdup;

src/cmd/ksh93/sh/xec.c
@@ -952,6 +952,7 @@ int sh_exec(register const Shnode_t *t, int flags)
 			sh_offstate(SH_ERREXIT);
 		sh.exitval=0;
 		sh.lastsig = 0;
+		sh.chldexitsig = 0;
 		sh.lastpath = 0;
 		switch(type&COMMSK)
 		{
@@ -1854,8 +1855,6 @@ int sh_exec(register const Shnode_t *t, int flags)
 				sh_popcontext(buffp);
 				if(jmpval > SH_JMPEXIT)
 					siglongjmp(*sh.jmplist,jmpval);
-				if(sh.exitval > 256)
-					sh.exitval -= 128;
 				sh_done(0);
 			}
 			else

src/cmd/ksh93/tests/subshell.sh
@@ -1154,4 +1154,22 @@ exp=OK0
 	"(expected $(printf %q "$exp"), got $(printf %q "$got"))"
 
 # ======
+# A virtual subshell should trim its exit status to 8 bits just like a real subshell, but if
+# its last command was killed by a signal then that should still be reflected in the 9th bit.
+sig=USR1
+exp=$((${ kill -l "$sig"; }|0x100))
+{ (:; "$SHELL" -c "kill -s $sig \$\$"); } 2>/dev/null
+let "(got=$?)==exp" || err_exit "command killed by signal in virtual subshell: expected status $exp, got status $got"
+{ (ulimit -t unlimited; "$SHELL" -c "kill -s $sig \$\$"); } 2>/dev/null
+let "(got=$?)==exp" || err_exit "command killed by signal in real subshell: expected status $exp, got status $got"
+{ (trap : EXIT; "$SHELL" -c "kill -s $sig \$\$"); } 2>/dev/null
+let "(got=$?)==exp" || err_exit "command killed by signal in virtual subshell with trap: expected status $exp, got status $got"
+{ (ulimit -t unlimited; trap : EXIT; "$SHELL" -c "kill -s $sig \$\$"); } 2>/dev/null
+let "(got=$?)==exp" || err_exit "command killed by signal in real subshell with trap: expected status $exp, got status $got"
+(:; exit "$exp")
+let "(got=$?)==(exp&0xFF)" || err_exit "fake signal exit from virtual subshell: expected status $((exp&0xFF)), got status $got"
+(ulimit -t unlimited 2>/dev/null; exit "$exp")
+let "(got=$?)==(exp&0xFF)" || err_exit "fake signal exit from real subshell: expected status $((exp&0xFF)), got status $got"
+
+# ======
 exit $((Errors<125?Errors:125))