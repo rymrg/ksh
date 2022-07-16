6c71c5ec4fe8d4adf56f679ef350e33e8454590b
Martijn Dekker <martijn@inlv.org>
2022-07-16 05:20:13 +0200

    Properly block .sh.level and .sh.value discipline functions
    These now give an "invalid discipline function" error instead of
    being silently ignored or crashing the shell.

--- src/cmd/ksh93/sh/nvdisc.c
+++ src/cmd/ksh93/sh/nvdisc.c
@@ -507,6 +507,8 @@ char *nv_setdisc(register Namval_t* np,register const char *event,Namval_t *acti
 	if (type < 0)
 		return(NIL(char*));
 	/* Handle GET/SET/APPEND/UNSET disc */
+	if(np==SH_VALNOD || np==SH_LEVELNOD)
+		return(0);
 	if(vp && vp->fun.disc->putval!=assign)
 		vp = 0;
 	if(!vp)
@@ -529,7 +531,7 @@ char *nv_setdisc(register Namval_t* np,register const char *event,Namval_t *acti
 		action = vp->disc[type];
 		empty = 0;
 	}
-	else if(action && np!=SH_LEVELNOD)
+	else if(action)
 	{
 		Namdisc_t *dp = (Namdisc_t*)vp->fun.disc;
 		dp->getval = lookup;
