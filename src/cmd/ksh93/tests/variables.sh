rymrg/ksh

commit 75b247cce236f02da354fcc124014b1b7ebc11ae

Honour attributes for local assignments preceding certain commands
Reproducer:

    $ typeset -i NUM=123
    $ (NUM=3+4 env; :)|grep ^NUM=
    NUM=3+4
    $ (NUM=3+4 env)|grep ^NUM=
    NUM=7

The correct output is NUM=7. This is also the output if ksh is
compiled with SHOPT_SPAWN disabled, suggesting that the problem is
here in sh_ntfork(), where the invocation-local environment list is
set using a sh_scope() call:

src/cmd/ksh93/sh/xec.c:
3496:	if(t->com.comset)
3497:	{
3498:		scope++;
3499:		sh_scope(t->com.comset,0);
3500:	}

Analysis:

When ksh is compiled with SHOPT_SPAWN disabled, external commands
are always run using the regular forking mechanism. First the shell
forks, then in the fork, the preceding assignments list (if any)
are executed and exported in the main scope. Replacing global
variables is not a problem as the variables are exported and the
forked shell is then replaced by the external command using
execve(2).

But when using SHOPT_SPAWN/sh_ntfork(), this cannot be done as the
fork(2) use is replaced by posix_spawn(2) which does not copy the
parent process into the child, therefore it's not possible to
execute anything in the child before calling execve(2). Which means
the preceding assignments list must be processed in the parent, not
the child. Which makes overwriting global variables a no-no.

To avoid overwriting global variables, sh_ntfork() treats preceding
assignments like local variables in functions, which means they do
not inherit any attributes from the parent scope. That is why the
integer attribute is not honoured in the buggy reproducers.

And this is not just an issue for external commands. sh_scope() is
also used for assignments preceding a built-in command. Which is
logical, as those don't create a process at all.

src/cmd/ksh93/sh/xec.c:
1325:	if(argp)
1326:	{
1327:		scope++;
1328:		sh_scope(argp,0);
1329:	}

Which means this bug exists for them as well, regardless of whether
SHOPT_SPAWN is compiled in.

src/cmd/ksh93/sh/name.c
@@ -253,7 +253,7 @@ void nv_setlist(register struct argnod *arg,register int flags, Namval_t *typ)
 	struct Namref	nr;
 	int		maketype = flags&NV_TYPE;  /* make a 'typeset -T' type definition command */
 	struct sh_type	shtp;
-	Dt_t		*save_vartree;
+	Dt_t		*vartree, *save_vartree;
 #if SHOPT_NAMESPACE
 	Namval_t	*save_namespace;
 #endif
@@ -287,6 +287,7 @@ void nv_setlist(register struct argnod *arg,register int flags, Namval_t *typ)
 	}
 	else
 		sh.prefix_root = sh.first_root = 0;
+	vartree = sh.prefix_root ? sh.prefix_root : sh.var_tree;
 	for(;arg; arg=arg->argnxt.ap)
 	{
 		sh.used_pos = 0;
@@ -474,8 +475,9 @@ void nv_setlist(register struct argnod *arg,register int flags, Namval_t *typ)
 						cp = stakcopy(nv_name(np));
 						if(!(arg->argflag&ARG_APPEND))
 							flag &= ~NV_ARRAY;
-						sh.prefix_root = sh.first_root;
-						np = nv_open(cp,sh.prefix_root?sh.prefix_root:sh.var_tree,flag);
+						if(sh.prefix_root = sh.first_root)
+							vartree = sh.prefix_root;
+						np = nv_open(cp,vartree,flag);
 					}
 					if(arg->argflag&ARG_APPEND)
 					{
@@ -563,20 +565,28 @@ void nv_setlist(register struct argnod *arg,register int flags, Namval_t *typ)
 			cp = arg->argval;
 			mp = 0;
 		}
-		if(eqp = strchr(cp,'='))
+		/*
+		 * Check if parent scope attributes need to be copied to the
+		 * temporary local scope of an assignment preceding a command.
+		 * https://github.com/ksh93/ksh/issues/465
+		 */
+		if((flags&(NV_EXPORT|NV_NOSCOPE))==(NV_EXPORT|NV_NOSCOPE) && dtvnext(vartree) && (eqp = strchr(cp,'=')))
 		{
-			/* Check for read-only */
 			*eqp = '\0';
-			np = nv_search(cp,sh.var_tree,0);
-			*eqp = '=';
-			if(np && nv_isattr(np,NV_RDONLY))
+			if((np = nv_search(cp,dtvnext(vartree),0)) && np->nvflag)
 			{
-				errormsg(SH_DICT,ERROR_exit(1),e_readonly,nv_name(np));
-				UNREACHABLE();
+				mp = nv_search(cp,vartree,NV_ADD|HASH_NOSCOPE);
+				mp->nvname = np->nvname;  /* put_lang() (init.c) compares nvname pointers */
+				mp->nvflag = np->nvflag;
+				mp->nvsize = np->nvsize;
+				mp->nvfun = np->nvfun;
 			}
-
+			*eqp = '=';
 		}
-		np = nv_open(cp,sh.prefix_root?sh.prefix_root:sh.var_tree,flags);
+		/*
+		 * Perform the assignment
+		 */
+		np = nv_open(cp,vartree,flags);
 		if(!np->nvfun && (flags&NV_NOREF))
 		{
 			if(sh.used_pos)

src/cmd/ksh93/sh/xec.c
@@ -3052,15 +3052,9 @@ pid_t sh_fork(int flags, int *jobid)
 static void  local_exports(register Namval_t *np, void *data)
 {
 	register Namval_t	*mp;
-	register char		*cp;
 	NOT_USED(data);
-	if(nv_isarray(np))
-		nv_putsub(np,NIL(char*),0);
-	if((cp = nv_getval(np)) && (mp = nv_search(nv_name(np), sh.var_tree, NV_ADD|HASH_NOSCOPE)) && nv_isnull(mp))
-	{
-		nv_putval(mp, cp, 0);
-		mp->nvflag = np->nvflag;
-	}
+	if(!nv_isnull(np) && (mp = nv_search(nv_name(np), sh.var_tree, NV_ADD|HASH_NOSCOPE)) && nv_isnull(mp))
+		nv_clone(np,mp,0);
 }
 
 /*

src/cmd/ksh93/tests/attributes.sh
@@ -800,4 +800,15 @@ got=$(export LC_NUMERIC=debug; typeset -xF5 num=7,75; "$SHELL" -c 'typeset -p nu
 	"(expected $(printf %q "$exp"), got $(printf %q "$got"))"
 
 # ======
+# Check that assignments preceding commands correctly honour existing attributes
+# https://github.com/ksh93/ksh/issues/465
+exp='typeset -x -F 5 num=7.75000'
+got=$(typeset -F5 num; num=3.25+4.5 "$SHELL" -c 'typeset -p num')
+[[ $got == "$exp" ]] || err_exit 'assignment preceding external command call does not honour pre-set attributes' \
+	"(expected $(printf %q "$exp"), got $(printf %q "$got"))"
+got=$(typeset -F5 num; num=3.25+4.5 command eval 'typeset -p num')
+[[ $got == "$exp" ]] || err_exit 'assignment preceding built-in command call does not honour pre-set attributes' \
+	"(expected $(printf %q "$exp"), got $(printf %q "$got"))"
+
+# ======
 exit $((Errors<125?Errors:125))

src/cmd/ksh93/tests/functions.sh
@@ -1274,10 +1274,18 @@ got=$( { "$SHELL" -c 'PATH=/dev/null; function fn { unset -f fn; true; }; fn; fn
 # ======
 # Check if environment variables passed while invoking a function are exported
 # https://github.com/att/ast/issues/32
-unset foo
-function f2 { env | grep -q "^foo" || err_exit "Environment variable is not propagated from caller function"; }
-function f1 { f2; env | grep -q "^foo" || err_exit "Environment variable is not passed to a function"; }
-foo=bar f1
+# https://github.com/ksh93/ksh/issues/465
+exp='typeset -x -F 5 num=7.75000'
+exp=$exp$'\n'$exp$'\n'$exp
+got=$(
+	function f1 { typeset -p num; f2; }
+	function f2 { typeset -p num; f3; }
+	function f3 { typeset -p num; }
+	typeset -F5 num
+	num=3.25+4.5 f1
+)
+[[ $got == "$exp" ]] || err_exit 'assignment preceding ksh function call is not correctly exported or propagated' \
+	"(expected $(printf %q "$exp"), got $(printf %q "$got"))"
 
 # ======
 # Over-shifting in a POSIX function should terminate the script

commit 13ab637a872ce19884aac744b73d9831e6721e70

Fix LINENO being wrong after being set in an invocation-local scope
LINENO is reset after being used in an invocation-local scope
(temporary assignment preceding a command). Reproducer:

	:
	LINENO=$LINENO true
	print "Line 3 is $LINENO"

Output: Line 3 is 1
Expect: Line 3 is 3

The bug was introoduced in ksh 93u- 2010-10-26 by adding LINENO to
the variables listed in nv_cover() in init.c. That function is used
to determine if a variable's discipline function (nvfun) should be
copied over when making a locally scoped version of it. The 93u-
changelog does not mention why this change was made.

Reverting it works for 93u+, but causes the above reproducer to
crash in 93u+m. A related 93u+m crash occurs with $RANDOM:

	$ ksh -c 'RANDOM=$RANDOM true; test -n $RANDOM'
	Memory fault

These crashes were introduced in 75b247cc by this line in name.c:
566:		mp->nvfun = np->nvfun;

Copying the variable's discipline function pointer nvfun should
only be done if nv_cover() indicates that it should be done.

src/cmd/ksh93/sh/init.c: nv_cover():
- Revert the 2010-10-26 addition of LINENO.

src/cmd/ksh93/sh/name.c: nv_setlist():
- For invocation-local scopes, only copy nvfun if nv_cover()
  indicates it for that variable, otherwise reset it.

Thanks to @JohnoKing for reporting and researching the bug.
Resolves: https://github.com/ksh93/ksh/issues/543

src/cmd/ksh93/include/version.h
@@ -18,7 +18,7 @@
 
 #define SH_RELEASE_FORK	"93u+m"		/* only change if you develop a new ksh93 fork */
 #define SH_RELEASE_SVER	"1.0.4-alpha"	/* semantic version number: https://semver.org */
-#define SH_RELEASE_DATE	"2022-09-27"	/* must be in this format for $((.sh.version)) */
+#define SH_RELEASE_DATE	"2022-09-28"	/* must be in this format for $((.sh.version)) */
 #define SH_RELEASE_CPYR	"(c) 2020-2022 Contributors to ksh " SH_RELEASE_FORK
 
 /* Scripts sometimes field-split ${.sh.version}, so don't change amount of whitespace. */

src/cmd/ksh93/sh/init.c
@@ -1709,7 +1709,7 @@ int sh_reinit(char *argv[])
  */
 Namfun_t *nv_cover(register Namval_t *np)
 {
-	if(np==IFSNOD || np==PATHNOD || np==SHELLNOD || np==FPATHNOD || np==CDPNOD || np==SECONDS || np==ENVNOD || np==LINENO)
+	if(np==IFSNOD || np==PATHNOD || np==SHELLNOD || np==FPATHNOD || np==CDPNOD || np==SECONDS || np==ENVNOD)
 		return(np->nvfun);
 #ifdef _hdr_locale
 	if(np==LCALLNOD || np==LCTYPENOD || np==LCMSGNOD || np==LCCOLLNOD || np==LCNUMNOD || np==LCTIMENOD || np==LANGNOD)

src/cmd/ksh93/sh/name.c
@@ -563,7 +563,7 @@ void nv_setlist(register struct argnod *arg,register int flags, Namval_t *typ)
 				mp->nvname = np->nvname;  /* put_lang() (init.c) compares nvname pointers */
 				mp->nvflag = np->nvflag;
 				mp->nvsize = np->nvsize;
-				mp->nvfun = np->nvfun;
+				mp->nvfun = nv_cover(np);
 			}
 			*eqp = '=';
 		}

src/cmd/ksh93/tests/variables.sh
@@ -1524,4 +1524,11 @@ got=$(one)
 	"(expected $(printf %q "$exp"), got $(printf %q "$got"))"
 
 # ======
+# https://github.com/ksh93/ksh/issues/543
+got=$("$SHELL" -c $':\nLINENO=$LINENO true\nprint "Line 3 is $LINENO"')
+exp='Line 3 is 3'
+[[ $got == "$exp" ]] || err_exit "LINENO is wrong after being set in an invocation-local scope" \
+	"(expected $(printf %q "$exp"), got $(printf %q "$got"))"
+
+# ======
 exit $((Errors<125?Errors:125))